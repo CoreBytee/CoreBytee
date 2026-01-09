7f1140f44756a9141bdae4e42838bc9a8c16e740
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-10 00:19:42 +0100

    remove old

--- src/script/bodyWidth.ts
+++
@@ -1,9 +0,0 @@
-function onBodyResize() {
-    document.body.style.setProperty(
-        '--body-width',
-        `${document.body.clientWidth}px`
-    )
-}
-
-onBodyResize()
-window.addEventListener('resize', onBodyResize)
